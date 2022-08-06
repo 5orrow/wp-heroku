09fce37a7fde7053658bf7cff2f1a8dcf2ff615d
5orrow <doggyxiong@gmail.com>
2022-08-06 23:02:47 +0700

    db config test

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'heroku_3d9940f51911db8' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'b7b8c0b6407af4' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'f33133cf' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql://b7b8c0b6407af4:f33133cf@us-cdbr-east-06.cleardb.net/heroku_3d9940f51911db8?reconnect=true' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
